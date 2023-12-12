ab72f4ab00d757d94cc795e52d4b20909c94626d
maffe <manfred.malmros@gmail.com>
2023-12-12 13:12:24 +0100

    Init commit

--- Labb4.py
+++ Labb4.py
@@ -11,9 +11,11 @@ for file in files:
     x = [pair[0] for pair in sorted_array]
     y = [pair[1] for pair in sorted_array]
     maxima = list(sp.signal.argrelmax(np.array(y)))
-    for maxima in maxima[0]:
-        plt.annotate(str(y[maxima]), (x[maxima], y[maxima]*1.05))
-        plt.plot(x[maxima], y[maxima], 'o', markerfacecolor='red', markeredgecolor='red', markersize=8, zorder=10)
+    maxima = maxima[0]
+    if len(maxima) > 0:
+        maxima = maxima[0]
+        plt.annotate("{:.2g}".format(y[maxima]*1000) + 'mA\n' + "{:.2g}".format(x[maxima]) + 'V', (x[maxima], y[maxima]*1.15), bbox=dict(facecolor='white', alpha=0.8))
+        plt.plot(x[maxima], y[maxima], 'o', markerfacecolor='red', markeredgecolor='black', markersize=8, zorder=10)
         print(y[maxima])
     plt.plot(x, y, '-b+')
     plt.grid(color='gray', linestyle='-')
